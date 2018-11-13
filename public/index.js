e8301b33868cc205ea2ab3bb1bdf03801067f9f5
miyamoen <akihito.biblioteca@gmail.com>
2018-11-13 21:40:46 +0900

    seperate frontend js

---
+++ public/index.js
@@ -0,0 +1,25 @@
+async function start() {
+  history.replaceState({}, "", "../");
+  const elmJsons = await readElmJsons();
+  const app = Elm.Main.init({ flags: Array.from(elmJsons) });
+  setupPorts(app);
+}
+
+const setupPorts = app => {
+  app.ports.fetchPackageDocs.subscribe(
+    async ({ authorName, packageName, version }) => {
+      const packageDocs = await readPackageDocs(
+        authorName,
+        packageName,
+        version
+      );
+
+      app.ports.acceptPackageDocs.send({
+        ...packageDocs,
+        authorName: authorName,
+        packageName: packageName,
+        version: version
+      });
+    }
+  );
+};
