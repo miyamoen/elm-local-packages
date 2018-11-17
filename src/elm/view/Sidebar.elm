daa9559caf48d12966a22a324bc8cbcf303c05b9
miyamoen <akihito.biblioteca@gmail.com>
2018-11-17 19:18:03 +0900

    Nice looking

--- src/elm/view/Sidebar.elm
+++ src/elm/view/Sidebar.elm
@@ -5,9 +5,10 @@ import Constant
 import Element exposing (..)
 import Element.Font as Font
 import Elm.Docs exposing (Module)
+import Elm.Version as Version
 import Fake
+import Route exposing (DocsKey)
 import Types exposing (..)
-import Url.Builder exposing (relative)
 import Util.AllDocs as AllDocs
 import Util.Route as Route
 import Util.Status as Status
@@ -16,35 +17,66 @@ import ViewUtil exposing (withCss)
 
 view : Model -> Maybe (Element msg)
 view { route, allDocs } =
-    Route.docsKey route
-        |> Maybe.andThen (\key -> AllDocs.find key allDocs)
-        |> Maybe.andThen Status.toMaybe
-        |> Maybe.map (.moduleDocs >> moduleList)
+    case Route.docsKey route of
+        Just key ->
+            AllDocs.find key allDocs
+                |> Maybe.andThen Status.toMaybe
+                |> Maybe.map (.moduleDocs >> moduleList key)
+
+        Nothing ->
+            Nothing
 
 
-moduleList : List Module -> Element msg
-moduleList moduleDocs =
+moduleList : DocsKey a -> List Module -> Element msg
+moduleList key moduleDocs =
     column [ spacing <| Constant.padding // 2 ]
-        [ el [ Font.size <| round <| Constant.fontSize * 1.5 ] <| text "Module Docs"
-        , column [] <| List.map moduleLink moduleDocs
+        [ readMeLink key
+        , el [ Font.size Constant.fontSize.middle ] <| text "Module Docs"
+        , column [] <| List.map (moduleLink key) moduleDocs
         ]
 
 
-moduleLink : Module -> Element msg
-moduleLink { name } =
+moduleLink : DocsKey a -> Module -> Element msg
+moduleLink key { name } =
     link
-        [ height <| px <| round <| Constant.fontSize * 1.5
-        , Font.color Constant.linkColor
-        , mouseOver [ Font.color Constant.accentColor ]
+        [ height <| px Constant.fontSize.middle
+        , Font.color Constant.color.link
+        , mouseOver [ Font.color Constant.color.accent ]
         ]
-        { label = text name
-        , url = relative [ String.replace "." "-" name ] []
+        { label = el [ centerY ] <| text name
+        , url =
+            Route.moduleAsString
+                { authorName = key.authorName
+                , packageName = key.packageName
+                , version = key.version
+                , moduleName = name
+                }
+        }
+
+
+readMeLink : DocsKey a -> Element msg
+readMeLink key =
+    link
+        [ height <| px Constant.fontSize.middle
+        , Font.color Constant.color.link
+        , mouseOver [ Font.color Constant.color.accent ]
+        ]
+        { label = el [ centerY ] <| text "README"
+        , url = Route.readMeAsString key
         }
 
 
 book : Book
 book =
-    bookWithFrontCover "Sidebar" (moduleList Fake.moduleDocs |> withCss)
+    bookWithFrontCover "Sidebar" (moduleList fakeKey Fake.moduleDocs |> withCss)
+
+
+fakeKey : DocsKey {}
+fakeKey =
+    { authorName = "miyamoen"
+    , packageName = "select-list"
+    , version = Version.fromString "4.0.0" |> Maybe.withDefault Version.one
+    }
 
 
 main : Bibliopola.Program
