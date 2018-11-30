db262672cb43094bb89506c78c8d50d69c02309b
miyamoen <akihito.biblioteca@gmail.com>
2018-11-30 23:44:06 +0900

    Status

--- src/elm/Views/Atoms/Status.elm
+++ src/elm/Views/Atoms/Status.elm
@@ -1,4 +1,4 @@
-module Status exposing (view, book)
+module Views.Atoms.Status exposing (view, book)
 
 {-|
 
@@ -10,7 +10,7 @@ import Bibliopola exposing (..)
 import Bibliopola.Story as Story
 import Element exposing (..)
 import Types exposing (Status(..))
-import ViewUtil exposing (withCss)
+import Views.Utils exposing (withFrame)
 
 
 view : (a -> Element msg) -> Status a -> Element msg
@@ -28,16 +28,16 @@ view successView status =
 
 book : Book
 book =
-    intoBook "Status" identity (view (always <| text "Success") >> withCss)
+    intoBook "Status" identity (view (always <| text "Success") >> withFrame)
         |> addStory
             (Story "status"
-                [ ( "Success", Success 1 )
+                [ ( "Success", Success () )
                 , ( "Failure", Failure )
                 , ( "Loading", Loading )
                 ]
             )
         |> buildBook
-        |> withFrontCover (view (always <| text "Success") Loading |> withCss)
+        |> withFrontCover (view (always <| text "Success") Loading |> withFrame)
 
 
 main : Bibliopola.Program
--- src/elm/Views/Bib.elm
+++ src/elm/Views/Bib.elm
@@ -3,13 +3,18 @@ module Views.Bib exposing (main, shelf)
 import Bibliopola exposing (..)
 import Views.Atoms.Logo
 import Views.Atoms.MarkdownBlock
+import Views.Atoms.Status
 
 
 shelf : Shelf
 shelf =
     emptyShelf "elm-local-packages"
-        |> addBook Views.Atoms.Logo.book
-        |> addBook Views.Atoms.MarkdownBlock.book
+        |> addShelf
+            (emptyShelf "Atoms"
+                |> addBook Views.Atoms.Logo.book
+                |> addBook Views.Atoms.MarkdownBlock.book
+                |> addBook Views.Atoms.Status.book
+            )
 
 
 main : Bibliopola.Program
